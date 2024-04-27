de01640e6d667728ba1f254cab00a6d5a642d917
Fernando Ayats <ayatsfer@gmail.com>
2024-04-27 17:04:47 +0200

    use ndg for docs

--- flake.nix
+++ flake.nix
@@ -1,86 +1,65 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    ndg = {
+      url = "github:feel-co/ndg";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
+    ndg,
   }: let
     forAllSystems = function:
-      nixpkgs.lib.genAttrs [
+      nixpkgs.lib.genAttrs
+      [
         "x86_64-linux"
         "aarch64-linux"
-      ] (system:
-        function
-        # Import nixpkgs to try google-chrome wrapper
-        # this pkgs is not used by the consumer, only .lib
-        (import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        }));
-
-    doc = forAllSystems (pkgs:
-      import ./doc {
-        inherit pkgs;
-        optionsCommonMark = self.legacyPackages.${pkgs.system}.optionsCommonMark;
-      });
+      ]
+      (
+        system:
+          function
+          # Import nixpkgs to try google-chrome wrapper
+          # this pkgs is not used by the consumer, only .lib
+          (
+            import nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            }
+          )
+      );
   in
-    (
-      import ./default.nix {
-        inherit (nixpkgs) lib;
-      }
-    )
+    (import ./default.nix {inherit (nixpkgs) lib;})
     // {
       formatter = forAllSystems (pkgs: pkgs.alejandra);
 
-      checks = forAllSystems (pkgs:
-        (self.lib {
-          inherit pkgs;
-          modules = [./tests/test-module.nix];
-          specialArgs = {
-            some-special-arg = "foo";
-          };
-        })
-        .config
-        .build
-        .packages);
-
-      packages = forAllSystems (pkgs: doc.${pkgs.system}.packages);
-
-      devShells = forAllSystems (pkgs: doc.${pkgs.system}.devShells);
-
-      legacyPackages = forAllSystems (
+      checks = forAllSystems (
         pkgs:
-          pkgs.nixosOptionsDoc {
-            options =
-              (self.lib {
-                inherit pkgs;
-                modules = [
-                  {
-                    options._module.args = pkgs.lib.mkOption {internal = true;};
-                  }
-                ];
-              })
-              .options;
-            transformOptions = opt:
-              opt
-              // {
-                declarations = with pkgs.lib;
-                  map
-                  (decl:
-                    if hasPrefix (toString ./.) (toString decl)
-                    then let
-                      rev = self.rev or "master";
-                      subpath = removePrefix "/" (removePrefix (toString ./.) (toString decl));
-                    in {
-                      url = "https://github.com/viperML/wrapper-manager/blob/${rev}/${subpath}";
-                      name = subpath;
-                    }
-                    else decl)
-                  opt.declarations;
-              };
-          }
+          (self.lib {
+            inherit pkgs;
+            modules = [./tests/test-module.nix];
+            specialArgs = {
+              some-special-arg = "foo";
+            };
+          })
+          .config
+          .build
+          .packages
+      );
+
+      packages = forAllSystems (
+        pkgs: {
+          doc = ndg.packages.${pkgs.system}.ndg-builder.override {
+            evaluatedModules = self.lib {
+              inherit pkgs;
+              modules = [];
+            };
+          };
+        }
       );
     };
 }
